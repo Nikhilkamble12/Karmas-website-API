e3ddbe3a536d2f63a8a6de4f040a0e056a1e4d9e
Kewal-Mverse <kewal.patel@mversesolution.com>
2024-12-25 18:15:54 +0530

    update view.fields.contants.js

--- server/utils/db/view.fields.contants.js
+++ server/utils/db/view.fields.contants.js
@@ -4,7 +4,7 @@ const ViewFieldTableWise = {
     //  ------> C
     CITY_MASTER_FIELDS:` SELECT city_id, city_name, country_id, country_name, state_id, state_name, is_metro_city, is_active FROM ${VIEW_NAME.GET_ALL_CITY_MASTER} ` ,
     COUNTRY_MASTER_FIELDS:` SELECT country_id, country_name, is_active FROM ${VIEW_NAME.GET_ALL_COUNTRY_MASTER} `,
-    STATE_MASTER_FIELDS:` SELECT state_id, state_name, county_id, is_active ${VIEW_NAME.STATE_MASTER_FIELDS} `,
+    STATE_MASTER_FIELDS:` SELECT state_id, state_name, county_id, is_active ${VIEW_NAME.GET_ALL_STATE_MASTER} `,
 
     //  ------> U
     USER_MASTER_FIELDS:` SELECT user_id, user_name, password, full_name, email_id FROM ${VIEW_NAME.GET_ALL_USER_MASTER} `
