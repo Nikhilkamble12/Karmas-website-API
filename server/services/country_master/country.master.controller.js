e4b20015875f80036d091cdac58be2029cb1a58b
Kewal-Mverse <kewal.patel@mversesolution.com>
2025-01-19 15:59:14 +0530

    update authJwt.js, country.master.controller.js, country.master.route.js and country.master.service.js

---
+++ server/services/country_master/country.master.controller.js
@@ -0,0 +1,279 @@
+import countryMasterService from "./country.master.service.js";
+import commonPath from "../../middleware/comman_path/comman.path.js";
+const {commonResponse,responseCode,responseConst,logger,tokenData,currentTime,addMetaDataWhileCreateUpdate} = commonPath
+
+const CountryMasterController = {
+    // Create A new Record 
+    create: async (req, res) => {
+        try {
+            const data = req.body;
+            // Add metadata for creation (created by, created at)
+            await addMetaDataWhileCreateUpdate(data, req, res, false);
+            // data.created_by=1,
+            // data.created_at = new Date()
+            // Create the record using ORM
+            const createData = await countryMasterService.createService(data);
+            if (createData) {
+                return res
+                    .status(responseCode.CREATED)
+                    .send(
+                        commonResponse(
+                            responseCode.CREATED,
+                            responseConst.SUCCESS_ADDING_RECORD
+                        )
+                    );
+            } else {
+                return res
+                    .status(responseCode.BAD_REQUEST)
+                    .send(
+                        commonResponse(
+                            responseCode.BAD_REQUEST,
+                            responseConst.ERROR_ADDING_RECORD,
+                            null,
+                            true
+                        )
+                    );
+            }
+        } catch (error) {
+            console.log("error",error)
+            logger.error(`Error ---> ${error}`);
+            return res
+                .status(responseCode.INTERNAL_SERVER_ERROR)
+                .send(
+                    commonResponse(
+                        responseCode.INTERNAL_SERVER_ERROR,
+                        responseConst.INTERNAL_SERVER_ERROR,
+                        null,
+                        true
+                    )
+                );
+        }
+    }, 
+    // update Record Into Db
+    update: async (req, res) => {
+        try {
+            const id = req.query.id
+            const data = req.body
+            // Add metadata for modification (modified by, modified at)
+            await addMetaDataWhileCreateUpdate(data, req, res, false);
+
+            // Update the record using ORM
+            const updatedRowsCount = await countryMasterService.updateService(id, data);
+            // if (updatedRowsCount > 0) {
+            //     const newData = await countryMasterService.getServiceById(id);
+            //     // Update the JSON data in the file
+            //     await CommanJsonFunction.updateDataByField(CITY_FOLDER, CITY_JSON, "city_id", id, newData, CITY_VIEW_NAME);
+            // }
+            // Handle case where no records were updated
+            if (updatedRowsCount === 0) {
+                return res
+                    .status(responseCode.BAD_REQUEST)
+                    .send(
+                        commonResponse(
+                            responseCode.BAD_REQUEST,
+                            responseConst.ERROR_UPDATING_RECORD,
+                            null,
+                            true
+                        )
+                    );
+            }
+            return res
+                .status(responseCode.CREATED)
+                .send(
+                    commonResponse(
+                        responseCode.CREATED,
+                        responseConst.SUCCESS_UPDATING_RECORD
+                    )
+                );
+        } catch (error) {
+            logger.error(`Error ---> ${error}`);
+            return res
+                .status(responseCode.INTERNAL_SERVER_ERROR)
+                .send(
+                    commonResponse(
+                        responseCode.INTERNAL_SERVER_ERROR,
+                        responseConst.INTERNAL_SERVER_ERROR,
+                        null,
+                        true
+                    )
+                );
+        }
+    },
+    // Retrieve all records 
+    getAllByView: async (req, res) => {
+        try {
+            // Fetch local data from JSON
+            // const GetAllJson = await CommanJsonFunction.getAllData(CITY_FOLDER,CITY_JSON)
+            // if(GetAllJson!==null){
+            //     if(GetAllJson.length!==0){
+            //       return res
+            //       .status(responseCode.OK)
+            //       .send(
+            //         commonResponse(
+            //           responseCode.OK,
+            //           responseConst.DATA_RETRIEVE_SUCCESS,
+            //           GetAllJson
+            //         )
+            //       );
+            //     }
+            //   }
+            // Fetch data from the database if JSON is empty
+            const getAll = await countryMasterService.getAllService()
+
+            // const fileStatus=await CommanJsonFunction.checkFileExistence(CITY_FOLDER,CITY_JSON)
+            // // Store the data in JSON for future retrieval
+            // if(fileStatus==false){
+            //   const DataToSave=await countryMasterService.getAllService()
+            //   if(DataToSave.length!==0){
+            //     await CommanJsonFunction.storeData( CITY_FOLDER, CITY_JSON, DataToSave, null, CITY_VIEW_NAME)
+            //   }
+            // }
+            // Return fetched data or handle case where no data is found
+            if (getAll.length !== 0) {
+                return res
+                    .status(responseCode.OK)
+                    .send(
+                        commonResponse(
+                            responseCode.OK,
+                            responseConst.DATA_RETRIEVE_SUCCESS,
+                            getAll
+                        )
+                    );
+            } else {
+                return res
+                    .status(responseCode.BAD_REQUEST)
+                    .send(
+                        commonResponse(
+                            responseCode.BAD_REQUEST,
+                            responseConst.DATA_NOT_FOUND,
+                            null,
+                            true
+                        )
+                    );
+            }
+        } catch (error) {
+            logger.error(`Error ---> ${error}`);
+            return res
+                .status(responseCode.INTERNAL_SERVER_ERROR)
+                .send(
+                    commonResponse(
+                        responseCode.INTERNAL_SERVER_ERROR,
+                        responseConst.INTERNAL_SERVER_ERROR,
+                        null,
+                        true
+                    )
+                );
+        }
+    },
+    // Retrieve a record by its ID
+    getByIdByView: async (req, res) => {
+        try {
+            const Id = req.query.id
+            // Fetch data by ID from JSON
+            // const getJsonDatabyId=await CommanJsonFunction.getFirstDataByField(CITY_FOLDER,CITY_JSON,"city_id",Id)
+            // if(getJsonDatabyId!==null){
+            //   return res
+            //     .status(responseCode.OK)
+            //     .send(
+            //       commonResponse(
+            //         responseCode.OK,
+            //         responseConst.DATA_RETRIEVE_SUCCESS,
+            //         getJsonDatabyId
+            //       )
+            //     );
+            // }
+
+            // If not found in JSON, fetch data from the database
+            const getDataByid = await countryMasterService.getServiceById(Id)
+
+            // const fileStatus=await CommanJsonFunction.checkFileExistence(CITY_FOLDER,CITY_JSON)
+            // // Store the data in JSON for future retrieval
+            // if(fileStatus==false){
+            //   const DataToSave=await countryMasterService.getAllService()
+            //   if(DataToSave.length!==0){
+            //     await CommanJsonFunction.storeData( CITY_FOLDER, CITY_JSON, DataToSave, null, CITY_VIEW_NAME)
+            //   }
+            // }
+            // Return the fetched data or handle case where no data is found
+            if (getDataByid.length !== 0) {
+                return res
+                    .status(responseCode.OK)
+                    .send(
+                        commonResponse(
+                            responseCode.OK,
+                            responseConst.DATA_RETRIEVE_SUCCESS,
+                            getDataByid
+                        )
+                    );
+            } else {
+                return res
+                    .status(responseCode.BAD_REQUEST)
+                    .send(
+                        commonResponse(
+                            responseCode.BAD_REQUEST,
+                            responseConst.DATA_NOT_FOUND,
+                            null,
+                            true
+                        )
+                    );
+            }
+        } catch (error) {
+            logger.error(`Error ---> ${error}`);
+            return res
+                .status(responseCode.INTERNAL_SERVER_ERROR)
+                .send(
+                    commonResponse(
+                        responseCode.INTERNAL_SERVER_ERROR,
+                        responseConst.INTERNAL_SERVER_ERROR,
+                        null,
+                        true
+                    )
+                );
+        }
+    },
+    // Delete A Record 
+    deleteData: async (req, res) => {
+        try {
+            const id = req.query.id
+            // Delete data from the database
+            const deleteData = await countryMasterService.deleteByid(id, req, res)
+            // Also delete data from the JSON file
+            // const deleteSatus=await CommanJsonFunction.deleteDataByField(CITY_FOLDER,CITY_JSON,"city_id",id)
+            if (deleteData === 0) {
+                return res
+                    .status(responseCode.BAD_REQUEST)
+                    .send(
+                        commonResponse(
+                            responseCode.BAD_REQUEST,
+                            responseConst.ERROR_DELETING_RECORD,
+                            null,
+                            true
+                        )
+                    );
+            }
+
+            return res
+                .status(responseCode.CREATED)
+                .send(
+                    commonResponse(
+                        responseCode.CREATED,
+                        responseConst.SUCCESS_DELETING_RECORD
+                    )
+                );
+        } catch (error) {
+            logger.error(`Error ---> ${error}`);
+            return res
+                .status(responseCode.INTERNAL_SERVER_ERROR)
+                .send(
+                    commonResponse(
+                        responseCode.INTERNAL_SERVER_ERROR,
+                        responseConst.INTERNAL_SERVER_ERROR,
+                        null,
+                        true
+                    )
+                );
+        }
+    }
+}
+
+export default CountryMasterController
--- server/services/country_master/country.master.route.js
+++ server/services/country_master/country.master.route.js
@@ -1,46 +1,43 @@
-// import StateMasterController from "./state.master.controller.js";
+// import CountryMasterController from "./state.master.controller.js";
+import CountryMasterController from "./country.master.controller.js";
 import commonPath from "../../middleware/comman_path/comman.path.js"; // Import common paths and utilities
 const {express,verifyToken,basePathRoute} = commonPath
 
 // Define the base path for routes
-const basePath=`${basePathRoute}/counrty_master`
+const basePath=`${basePathRoute}/country_master`
 const router = express.Router()
 // Route to create a new record
 
-// router.post(
-//     `${basePath}/create`,
-//     // verifyToken,
-//     StateMasterController.create
-// )
-// // Route to update an existing record by ID
-// router.put(
-//     `${basePath}/update`,
-//     verifyToken,
-//     StateMasterController.update
-// )
-// // Route to retrieve all records
-// router.get(
-//     `${basePath}`,
-//     verifyToken,
-//     StateMasterController.getAllByView
-// )
-// // Route to retrieve a record by ID
-// router.get(
-//     `${basePath}/getById`,
-//     verifyToken,
-//     StateMasterController.getByIdByView
-// )
-// // Route to delete a record by ID
-// router.delete(
-//     `${basePath}/delete`,
-//     verifyToken,
-//     StateMasterController.deleteData
-// )
-// router.get(
-//     `${basePath}/getByCountry`,
-//     verifyToken,
-//     StateMasterController.getStateByCountryId
-// )
+router.post(
+    `${basePath}/create`,
+    // verifyToken,
+    CountryMasterController.create
+)
+// Route to update an existing record by ID
+router.put(
+    `${basePath}/update`,
+    verifyToken,
+    CountryMasterController.update
+)
+// Route to retrieve all records
+router.get(
+    `${basePath}`,
+    verifyToken,
+    CountryMasterController.getAllByView
+)
+// Route to retrieve a record by ID
+router.get(
+    `${basePath}/getById`,
+    verifyToken,
+    CountryMasterController.getByIdByView
+)
+// Route to delete a record by ID
+router.delete(
+    `${basePath}/delete`,
+    verifyToken,
+    CountryMasterController.deleteData
+)
+
 
 // Export the router for use in other parts of the application
 export default router
