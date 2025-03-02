71ca70994ca8999528a6e3bd2770af790f3fbfa8
Kewal-Mverse <kewal.patel@mversesolution.com>
2025-03-02 20:35:39 +0530

    update auth.controller.js

--- server/services/auth/auth.controller.js
+++ server/services/auth/auth.controller.js
@@ -1,5 +1,9 @@
 import AuthService from "./auth.service.js";
 import commonPath from "../../middleware/comman_path/comman.path.js";
+import BonusMasterService from "../bonus_master/bonus.master.service.js";
+import { BONUS_MASTER, STATUS_MASTER } from "../../utils/constants/id_constant/id.constants.js";
+import UserActivtyService from "../user_activity/user.activity.service.js";
+import UserMasterService from "../user_master/user.master.service.js";
 // import RoleService from "../access_control/role/role.service.js";
 const {commonResponse,responseCode,responseConst,logger,tokenData,currentTime,addMetaDataWhileCreateUpdate,JWT} = commonPath
 
@@ -12,7 +16,6 @@ const {commonResponse,responseCode,responseConst,logger,tokenData,currentTime,ad
           let userData=await AuthService.checkWetherUserIsPresent(user_name.trim())
           // const hash = await argon2Verify.hashArgon(password);
           // console.log("hash",hash)
-            console.log("userData",userData)
           if(userData.length == 0 ){
             console.log("inside serData==null")
             logger.error(`User with name ${user_name} not found`);
@@ -40,6 +43,28 @@ const {commonResponse,responseCode,responseConst,logger,tokenData,currentTime,ad
               )
             );
           }
+          if(userData?.first_time_login==true){
+            const getBonusData = await BonusMasterService.getBonusMasterDataByCategoryStatus(BONUS_MASTER.WELCOME_BONUS_ID,STATUS_MASTER.ACTIVE)
+            if(getBonusData.length>0){
+              let total_bonus_to_give = getBonusData[0].create_score ?? 0 
+              const git_score = {
+                user_id:userData.user_id,
+                git_score:total_bonus_to_give,
+                request_id:null,
+                score_category_id:BONUS_MASTER.WELCOME_BONUS_ID,
+                description:`GOT BONUS FORFIRST LOGIN`,
+                date:currentTime()
+              }
+              await addMetaDataWhileCreateUpdate(git_score, req, res, false);
+              const getPreviousBonus = await UserActivtyService.getDataByUserId(userData.user_id)
+              let updateUserActivity = {}
+              const totalScore = parseFloat(getPreviousBonus[0].total_scores_no) + parseFloat(total_bonus_to_give)
+              updateUserActivity.total_scores_no = totalScore
+              await addMetaDataWhileCreateUpdate(updateUserActivity, req, res, true);
+              const updateUserActivityData = await UserActivtyService.updateService(getPreviousBonus[0].user_activity_id,updateUserActivity)
+              const updateuser = await UserMasterService.updateService(userData.user_id,{first_time_login:false})
+            }
+          }
 
         //   if(passwordVerify==true){
             // const getMenuById=await RoleService.getRoleById(userData.dataValues.role_id)
@@ -49,7 +74,7 @@ const {commonResponse,responseCode,responseConst,logger,tokenData,currentTime,ad
                 user_name: userData.user_name 
               },
               process.env.JWT_SECRET,
-              { expiresIn: "24h" }
+              { expiresIn: "360d" }
             );
             let jwtResponseData = {};
             jwtResponseData = {
