6662b8b943d5272b6a3fa322a1925b3724bae3b8
Nishant More <nishantmore228@gmail.com>
2025-03-25 23:22:54 +0530

    add Comments functionality with CRUD operations and database integration

---
+++ server/services/Comments/comments.model.js
@@ -0,0 +1,79 @@
+import { DataTypes } from "sequelize";  
+
+const CommentsModel = (sequelize)=>{
+  return sequelize.define(
+    "Comments",
+    {
+      comment_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      post_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      comment_text: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      parent_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      is_active: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      created_by: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      modified_by: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      modified_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      deleted_by: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      deleted_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "Comments",
+      paranoid: true,
+      timestamps: false,
+      defaultScope: {
+        attributes: {
+          exclude: [
+            "created_by",
+            "modified_by",
+            "deleted_by",
+            "deleted_at",
+            "is_active",
+            "created_at",
+            "modified_at",
+          ],
+        },
+      },
+    }
+  )
+}
+
+export default CommentsModel;
