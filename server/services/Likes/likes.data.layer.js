a91c11f5adabd674242b6296f455e3b216c47fde
Nishant More <nishantmore228@gmail.com>
2025-03-25 23:21:32 +0530

    add Likes functionality with CRUD operations and database integration

---
+++ server/services/Likes/likes.data.layer.js
@@ -0,0 +1,69 @@
+import LikesModel from "./likes.model.js";
+import commonPath from "../../middleware/comman_path/comman.path.js"; // Import common paths and utilities
+const { db, ViewFieldTableVise, tokenData } = commonPath; // Destructure necessary components from commonPath
+
+const LikesDAL = {
+  // Method to create a new record in the database
+  CreateData: async (data) => {
+    try {
+      const createdData = await LikesModel(db.sequelize).create(data);
+      return createdData; // Return the created data
+    } catch (error) {
+      throw error; // Throw error for handling in the controller
+    }
+  },
+  // Method to update an existing record by its ID
+  UpdateData: async (like_id, data) => {
+    try {
+      // console.log("data", data);
+      const updateData = await LikesModel(db.sequelize).update(data, {
+        where: { like_id: like_id },
+      }); // Return the result of the update operation
+      return updateData;
+    } catch (error) {
+      throw error; // Throw error for handling in the controller
+    }
+  },
+  // Method to retrieve all records by view
+  getAllDataByView: async () => {
+    try {
+      const getAllData = await db.sequelize.query(
+        `${ViewFieldTableVise.LIKES_FIELDS}`,
+        { type: db.Sequelize.QueryTypes.SELECT } // Return the retrieved data
+      );
+      return getAllData;
+    } catch (error) {
+      throw error; // Throw error for handling in the controller
+    }
+  },
+  // Method to retrieve a specific record by its ID
+  getDataByIdByView: async (like_id) => {
+    try {
+      const getDataById = await db.sequelize.query(
+        ` ${ViewFieldTableVise.LIKES_FIELDS} where like_id  = ${like_id} `,
+        { type: db.Sequelize.QueryTypes.SELECT } // Return the retrieved data
+      );
+      return getDataById[0] ?? [];
+    } catch (error) {
+      throw error; // Throw error for handling in the controller
+    }
+  },
+  // Method to mark a record as deleted (soft delete)
+  deleteDataById: async (like_id, req, res) => {
+    try {
+      const [deleteDataById] = await LikesModel(db.sequelize).update(
+        {
+          is_active: 0,
+          deleted_by: tokenData(req, res),
+          deleted_at: new Date(),
+        },
+        { where: { like_id: like_id } }
+      );
+      return deleteDataById;
+    } catch (error) {
+      throw error; // Throw error for handling in the controller
+    }
+  },
+};
+
+export default LikesDAL; // Export the LikesDAL object for use in the service
---
+++ server/services/Likes/likes.service.js
@@ -0,0 +1,46 @@
+import LikesDAL from "./likes.data.layer.js";
+
+const LikesService = {
+  // Method to create a new record
+  createSerive: async (data) => {
+    try {
+      return await LikesDAL.CreateData(data);
+    } catch (error) {
+      throw error;
+    }
+  },
+  // Method to update an existing record by its ID
+  updateService: async (like_id, data) => {
+    try {
+      return await LikesDAL.UpdateData(like_id, data);
+    } catch (error) {
+      throw error;
+    }
+  },
+  // Method to retrieve all records
+  getAllService: async () => {
+    try {
+      return await LikesDAL.getAllDataByView();
+    } catch (error) {
+      throw error;
+    }
+  },
+  // Method to retrieve a specific record by its ID
+  getServiceById: async (like_id) => {
+    try {
+      return await LikesDAL.getDataByIdByView(like_id);
+    } catch (error) {
+      throw error;
+    }
+  },
+  // Method to mark a record as deleted (soft delete) by its
+  deleteById: async (like_id, req, res) => {
+    try {
+      return await LikesDAL.deleteDataById(like_id, req, res);
+    } catch (error) {
+      throw error;
+    }
+  },
+}
+
+export default LikesService;
