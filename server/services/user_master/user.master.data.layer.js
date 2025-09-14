2cdbae866c16e7dfd17bd6d8fbb2d3507926b5ab
Rohan-mverse <rohan.sapkal@mversesolution.com>
2025-09-14 19:53:41 +0530

    feat: update SQL queries to include is_blacklisted field in user master data retrieval

--- server/services/user_master/user.master.data.layer.js
+++ server/services/user_master/user.master.data.layer.js
@@ -49,7 +49,7 @@ const UserMasterDAL = {
     // Method to retrieve all records by view
     getAllDataByView: async () => {
         try {
-            const getAllData = await db.sequelize.query(`${ViewFieldTableVise.USER_MASTER_FIELDS} where is_blacklisted = false`, { type: db.Sequelize.QueryTypes.SELECT })
+            const getAllData = await db.sequelize.query(`${ViewFieldTableVise.USER_MASTER_FIELDS}`, { type: db.Sequelize.QueryTypes.SELECT })
             return getAllData // Return the retrieved data
         } catch (error) {
             throw error // Throw error for handling in the controller
