c15412f80d1a465e29ec4c34b4a92dffd1b47c81
Kewal Patel <kwlptl123@gmail.com>
2026-01-19 16:38:19 +0530

    update server/services/index.js

--- server/services/index.js
+++ server/services/index.js
@@ -71,6 +71,7 @@ console.log("environment",environment)
 // Select the appropriate configuration based on the environment
 
 const config = await dbConfig[environment];
+console.log("config",config)
 // Initialize a new Sequelize instance with the selected configuration
 // const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
 //   host: config.HOST,
